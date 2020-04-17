5f7c96eb96ce2c0eba86f04f8dd15b4971ac699f
fatih.hyuseinov <f.hyuseinov@gmail.com>
2020-04-17 12:14:46 +0300

    crud users

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,12 @@ const express = require('express');
 const api = express.Router();
 
 api.use('/rentals', require('./rentals'));
-api.use('/users', require('./users'));
+api.use('/users', async (req, res, next) => {
+    if (req.auth.user.role !== 'admin') {
+        return next({ statusCode: 403, message: `You don't have permissions!` });
+    }
+
+    next();
+}, require('./users'));
 
 module.exports = api;
