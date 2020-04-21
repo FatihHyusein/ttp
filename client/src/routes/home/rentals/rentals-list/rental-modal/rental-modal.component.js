3a5a174bfd502d008531bac906deafa84892f58c
Dimitar <dd@globld.bg>
2020-04-21 18:09:18 +0300

    fixed rentals data issue

--- client/src/routes/home/rentals/rentals-list/rental-modal/rental-modal.component.js
+++ client/src/routes/home/rentals/rentals-list/rental-modal/rental-modal.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './rental-modal.component.scss';
 import { RentalsContext } from '../../../../../contexts/rentals.context';
 import { ajax } from '../../../../../core/ajax-requests.util';
-import { Button, Form, Input, InputNumber, message, Modal } from 'antd';
+import { Button, Form, Input, InputNumber, message, Modal, Switch } from 'antd';
 import { CoordinatesFormInputComponent } from './coordinates-form-input/coordinates-form-input.component';
 
 const layout = {
@@ -10,9 +10,103 @@ const layout = {
     wrapperCol: { span: 16 },
 };
 
-export function RentalModalComponent({ visible, closeModal, currentRental, googleMapInstances }) {
+function FormComponent({ closeModal, currentRental, googleMapInstances }) {
     const [form] = Form.useForm();
     const { setRentals } = React.useContext(RentalsContext);
+
+    return <Form
+        form={form}
+        id={'rental-form'}
+        initialValues={currentRental}
+        onFinish={(values) => {
+            function successHandle(responseJson) {
+                setRentals(responseJson);
+                closeModal();
+            }
+
+            currentRental._id ?
+
+                ajax.put({
+                    postData: values,
+                    url: `rentals/${currentRental._id}`
+                })
+                    .then(successHandle)
+                    .catch(error => {
+                        console.log(error);
+                        message.error('Could not update rental!');
+                    }) :
+
+                ajax.post({
+                    url: 'rentals',
+                    postData: values
+                })
+                    .then(successHandle)
+                    .catch(error => {
+                        console.log(error);
+                        message.error('Could not create rental!');
+                    });
+        }}
+        onFinishFailed={errorInfo => {
+            console.log('Failed:', errorInfo);
+        }}
+        hideRequiredMark={true}
+        {...layout}>
+        <Form.Item
+            label='Name'
+            name='name'
+            rules={[{ required: true, message: 'Please input name!' }]}
+        >
+            <Input/>
+        </Form.Item>
+        <Form.Item
+            label='Description'
+            name='description'
+            rules={[{ required: true, message: 'Please input description!' }]}
+        >
+            <Input.TextArea rows={4}/>
+        </Form.Item>
+        <Form.Item
+            label='Floor Area Size'
+            name='floorAreaSize'
+            rules={[{ required: true, message: 'Please input Floor Area Size!' }]}
+        >
+            <InputNumber min={1} max={9999}/>
+        </Form.Item>
+        <Form.Item
+            label='Price Per Month'
+            name='pricePerMonth'
+            rules={[{ required: true, message: 'Please input Price Per Month!' }]}
+        >
+            <InputNumber min={1} max={99999}/>
+        </Form.Item>
+        <Form.Item
+            label='Rooms Count'
+            name='roomsCount'
+            rules={[{ required: true, message: 'Please input Rooms Count!' }]}
+        >
+            <InputNumber min={1} max={99}/>
+        </Form.Item>
+        <CoordinatesFormInputComponent googleMapInstances={googleMapInstances} form={form}/>
+
+        <Form.Item
+            label='associatedRealtorId'
+            name='associatedRealtorId'
+            rules={[{ required: true, message: 'Please input associatedRealtorId!' }]}
+        >
+            <Input/>
+        </Form.Item>
+        <Form.Item
+            label='isAvailable'
+            name='isAvailable'
+            valuePropName='checked'
+        >
+            <Switch defaultChecked={true}/>
+        </Form.Item>
+    </Form>;
+}
+
+export function RentalModalComponent(props) {
+    const { visible, closeModal, currentRental } = props;
 
     return <Modal
         destroyOnClose={true}
@@ -27,87 +121,6 @@ export function RentalModalComponent({ visible, closeModal, currentRental, googl
                     htmlType='submit'>{currentRental._id ? 'Update' : 'Create'}</Button>,
         ]}
     >
-        <Form
-            form={form}
-            id={'rental-form'}
-            initialValues={currentRental}
-            onFinish={(values) => {
-                function successHandle(responseJson) {
-                    setRentals(responseJson);
-                    closeModal();
-                }
-
-                currentRental._id ?
-
-                    ajax.put({
-                        postData: values,
-                        url: `rentals/${currentRental._id}`
-                    })
-                        .then(successHandle)
-                        .catch(error => {
-                            console.log(error);
-                            message.error('Could not update rental!');
-                        }) :
-
-                    ajax.post({
-                        url: 'rentals',
-                        postData: values
-                    })
-                        .then(successHandle)
-                        .catch(error => {
-                            console.log(error);
-                            message.error('Could not create rental!');
-                        });
-            }}
-            onFinishFailed={errorInfo => {
-                console.log('Failed:', errorInfo);
-            }}
-            hideRequiredMark={true}
-            {...layout}>
-            <Form.Item
-                label='Name'
-                name='name'
-                rules={[{ required: true, message: 'Please input name!' }]}
-            >
-                <Input/>
-            </Form.Item>
-            <Form.Item
-                label='Description'
-                name='description'
-                rules={[{ required: true, message: 'Please input description!' }]}
-            >
-                <Input.TextArea rows={4}/>
-            </Form.Item>
-            <Form.Item
-                label='Floor Area Size'
-                name='floorAreaSize'
-                rules={[{ required: true, message: 'Please input Floor Area Size!' }]}
-            >
-                <InputNumber min={1} max={9999}/>
-            </Form.Item>
-            <Form.Item
-                label='Price Per Month'
-                name='pricePerMonth'
-                rules={[{ required: true, message: 'Please input Price Per Month!' }]}
-            >
-                <InputNumber min={1} max={99999}/>
-            </Form.Item>
-            <Form.Item
-                label='Rooms Count'
-                name='roomsCount'
-                rules={[{ required: true, message: 'Please input Rooms Count!' }]}
-            >
-                <InputNumber min={1} max={99}/>
-            </Form.Item>
-            <CoordinatesFormInputComponent googleMapInstances={googleMapInstances} form={form}/>
-
-            <Form.Item
-                label='associatedRealtorId'
-                name='associatedRealtorId'
-                rules={[{ required: true, message: 'Please input associatedRealtorId!' }]}
-            >
-                <Input/>
-            </Form.Item>
-        </Form>
+        <FormComponent {...props}/>
     </Modal>;
 }
--- server/api/rentals/index.js
+++ server/api/rentals/index.js
@@ -4,9 +4,17 @@ const { getAll, create, update, remove } = require('./rentals.api');
 const api = express.Router();
 
 
+async function preventClientHit(req, res, next) {
+    if (req.auth.user.role === 'customer') {
+        return next({ statusCode: 403, message: `You don't have permissions!` });
+    }
+
+    next();
+}
+
 api.get('/', getAll);
-api.post('/', create);
-api.put('/:rentalId', update);
-api.delete('/:rentalId', remove);
+api.post('/', preventClientHit, create);
+api.put('/:rentalId', preventClientHit, update);
+api.delete('/:rentalId', preventClientHit, remove);
 
 module.exports = api;
