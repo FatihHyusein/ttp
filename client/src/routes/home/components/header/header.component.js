46be2e100ce98fc6bf8a7f9f25326688b4472995
Dimitar <dd@globld.bg>
2020-04-21 22:17:26 +0300

    my profile modal

--- client/src/routes/home/components/header/header.component.js
+++ client/src/routes/home/components/header/header.component.js
@@ -9,50 +9,55 @@ import { AuthContext } from '../../../../contexts/auth.context';
 import { MyProfileContext } from '../../../../contexts/my-profile.context';
 import UserOutlined from '@ant-design/icons/lib/icons/UserOutlined';
 import LogoutOutlined from '@ant-design/icons/lib/icons/LogoutOutlined';
-import EditOutlined from '@ant-design/icons/lib/icons/EditOutlined';
+import InfoOutlined from '@ant-design/icons/lib/icons/InfoOutlined';
+import { MyProfileComponent } from '../my-profile/my-profile.component';
 
 const { Header } = Layout;
 
 
 export function HeaderComponent() {
+    const myProfileModalRef = React.useRef();
     const { setAuthToken } = React.useContext(AuthContext);
     const { myProfile } = React.useContext(MyProfileContext);
     const match = useRouteMatch();
     const location = useLocation();
     const selectedMenuItem = location.pathname.indexOf('/users') === 0 ? 'users' : 'rentals';
 
-    return <Header className={'page-header-component'}>
-        <Menu mode='horizontal' selectedKeys={[selectedMenuItem]}>
-            <Menu.Item key='rentals'>
-                <Link to={match.path}>
-                    <HomeOutlined/>
-                    Rentals
-                </Link>
-            </Menu.Item>
-            {myProfile.role === 'admin' ?
-                <Menu.Item key='users'>
-                    <Link to={`/users`}>
-                        <TeamOutlined/>
-                        Users
-                    </Link> </Menu.Item>
-                : null
-            }
-            <Menu.SubMenu
-                title={<span><UserOutlined/>{myProfile?.username}</span>}>
-                <Menu.ItemGroup>
-                    <Menu.Item key='my-profile' onClick={() => {
-
-                    }}><EditOutlined/> Edit</Menu.Item>
-                </Menu.ItemGroup>
-                <Menu.ItemGroup>
-                    <Menu.Item key='logout' onClick={() => {
-                        ajax.post({ url: 'auth/logout' }).then(() => {
-                            localStorage.token = '';
-                            setAuthToken(localStorage.token);
-                        });
-                    }}><LogoutOutlined/> Logout</Menu.Item>
-                </Menu.ItemGroup>
-            </Menu.SubMenu>
-        </Menu>
-    </Header>;
+    return <React.Fragment>
+        <Header className={'page-header-component'}>
+            <Menu mode='horizontal' selectedKeys={[selectedMenuItem]}>
+                <Menu.Item key='rentals'>
+                    <Link to={match.path}>
+                        <HomeOutlined/>
+                        Rentals
+                    </Link>
+                </Menu.Item>
+                {myProfile.role === 'admin' ?
+                    <Menu.Item key='users'>
+                        <Link to={`/users`}>
+                            <TeamOutlined/>
+                            Users
+                        </Link> </Menu.Item>
+                    : null
+                }
+                <Menu.SubMenu
+                    title={<span><UserOutlined/>{myProfile?.username}</span>}>
+                    <Menu.ItemGroup>
+                        <Menu.Item key='my-profile' onClick={() => {
+                            myProfileModalRef.current.open();
+                        }}><InfoOutlined/> Info</Menu.Item>
+                    </Menu.ItemGroup>
+                    <Menu.ItemGroup>
+                        <Menu.Item key='logout' onClick={() => {
+                            ajax.post({ url: 'auth/logout' }).then(() => {
+                                localStorage.token = '';
+                                setAuthToken(localStorage.token);
+                            });
+                        }}><LogoutOutlined/> Logout</Menu.Item>
+                    </Menu.ItemGroup>
+                </Menu.SubMenu>
+            </Menu>
+        </Header>
+        <MyProfileComponent ref={myProfileModalRef}/>
+    </React.Fragment>;
 }
