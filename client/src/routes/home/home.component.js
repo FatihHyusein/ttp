3e3ac55589afa6c674cc9c11e34e1858312c112b
fatih.hyuseinov <f.hyuseinov@gmail.com>
2020-04-21 14:21:01 +0300

    roles on the FE part

--- client/src/routes/home/home.component.js
+++ client/src/routes/home/home.component.js
@@ -1,66 +1,33 @@
 import React from 'react';
-import { Route, Switch, Link, useLocation, useRouteMatch } from 'react-router-dom';
+import { Route, Switch, Redirect, useRouteMatch } from 'react-router-dom';
 import './home.component.scss';
 import { UsersComponent } from './users/users.component';
 import { RentalsComponent } from './rentals/rentals.component';
-import { Layout, Menu } from 'antd';
-import HomeOutlined from '@ant-design/icons/lib/icons/HomeOutlined';
-import TeamOutlined from '@ant-design/icons/lib/icons/TeamOutlined';
-import SettingOutlined from '@ant-design/icons/lib/icons/SettingOutlined';
-import { ajax } from '../../core/ajax-requests.util';
-import { AuthContext } from '../../contexts/auth.context';
+import { Layout } from 'antd';
 import { MyProfileContext } from '../../contexts/my-profile.context';
+import { HeaderComponent } from './components/header/header.component';
+import { userRoles } from '../../core/roles.enum';
 
-const { Header, Footer, Content } = Layout;
+const { Content } = Layout;
 
 
 export function HomeComponent() {
-    const { setAuthToken } = React.useContext(AuthContext);
     const { myProfile } = React.useContext(MyProfileContext);
     const match = useRouteMatch();
-    const location = useLocation();
-    const selectedMenuItem = location.pathname.indexOf('/users') === 0 ? 'users' : 'rentals';
 
     return <div>
         <Layout>
-            <Header>
-                <Menu mode='horizontal' selectedKeys={[selectedMenuItem]}>
-                    <Menu.Item key='rentals'>
-                        <Link to={match.path}>
-                            <HomeOutlined/>
-                            Rentals
-                        </Link>
-                    </Menu.Item>
-                    <Menu.Item key='users'>
-                        <Link to={`/users`}>
-                            <TeamOutlined/>
-                            Users
-                        </Link> </Menu.Item>
-                    <Menu.SubMenu
-                        title={
-                            <span className='submenu-title-wrapper'>
-                                <SettingOutlined/>
-                              User
-                            </span>}>
-                        <Menu.ItemGroup>
-                            <Menu.Item key='my-profile'>{myProfile?.username}</Menu.Item>
-                        </Menu.ItemGroup>
-                        <Menu.ItemGroup>
-                            <Menu.Item key='logout' onClick={() => {
-                                ajax.post({ url: 'auth/logout' }).then(() => {
-                                    localStorage.token = '';
-                                    setAuthToken(localStorage.token);
-                                });
-                            }}>Logout</Menu.Item>
-                        </Menu.ItemGroup>
-                    </Menu.SubMenu>
-                </Menu>
-            </Header>
+            <HeaderComponent/>
             <Content>
                 <div className='site-layout-content'>
                     <Switch>
                         <Route exact path={match.path} component={RentalsComponent}/>
-                        <Route exactpath={`${match.path}/users`} component={UsersComponent}/>
+                        {
+                            myProfile.role === userRoles.admin ?
+                                <Route exactpath={`${match.path}/users`} component={UsersComponent}/>
+                                : null
+                        }
+                        <Route path='*' render={() => (<Redirect to={`/`}/>)}/>
                     </Switch>
                 </div>
             </Content>
--- client/src/routes/home/rentals/rentals-list/rentals-list.component.js
+++ client/src/routes/home/rentals/rentals-list/rentals-list.component.js
@@ -8,6 +8,8 @@ import { ajax } from '../../../../core/ajax-requests.util';
 import DeleteOutlined from '@ant-design/icons/lib/icons/DeleteOutlined';
 import { RentalModalComponent } from '../rental-modal/rental-modal.component';
 import { RentalsContext } from '../../../../contexts/rentals.context';
+import { MyProfileContext } from '../../../../contexts/my-profile.context';
+import { userRoles } from '../../../../core/roles.enum';
 
 
 const IconText = ({ icon, text, title }) => (
@@ -21,16 +23,61 @@ export function RentalsListComponent({ googleMapInstances }) {
     const [rentalModalVisible, setRentalModalVisible] = React.useState(false);
     const [currentRental, setCurrentRental] = React.useState({});
     const { rentals, setRentals } = React.useContext(RentalsContext);
+    const { myProfile } = React.useContext(MyProfileContext);
+
+    function getRentalActionSection(rental) {
+        const actionSection = [
+            <IconText title={'Date Added'} icon={CalendarOutlined}
+                      text={new Date(rental.dateAdded).toDateString()}
+                      key='size'/>,
+            <IconText title={'Floor Area Size'} icon={ExpandAltOutlined} text={rental.floorAreaSize}
+                      key='size'/>,
+            <IconText title={'Price per month'} icon={DollarOutlined} text={rental.pricePerMonth}
+                      key='price-per-month'/>,
+            <IconText title={'Number of rooms'} icon={AppstoreOutlined} text={rental.roomsCount}
+                      key='rooms'/>
+        ];
+
+        if (myProfile.role === userRoles.customer) {
+            return actionSection;
+        }
+
+        return [
+            ...actionSection,
+            <Button shape={'round'} type={'primary'} ghost icon={<EditOutlined/>}
+                    onClick={() => {
+                        setCurrentRental(rental);
+                        setRentalModalVisible(true);
+                    }}/>,
+            <Popconfirm
+                title='Sure to delete this rental?'
+                onConfirm={() => {
+                    ajax.delete({ url: `rentals/${rental._id}` }).then(responseJson => {
+                        setRentals(responseJson);
+                    });
+                }}
+                okText='Yes'
+                cancelText='No'
+            >
+                <Button shape={'round'} type={'danger'} ghost icon={<DeleteOutlined/>}/>
+            </Popconfirm>
+        ];
+
+    }
 
     return <React.Fragment>
         <List
             itemLayout='vertical'
-            header={<div className={'rentals-header'}><h1>Rentals</h1> <Button type={'primary'} icon={<AppstoreAddOutlined/>}
-                                         onClick={() => {
-                                             setCurrentRental({});
-                                             setRentalModalVisible(true);
-                                         }
-                                         }/></div>}
+            header={<div className={'rentals-header'}><h1>Rentals</h1>
+                {myProfile.role === userRoles.customer ?
+                    null
+                    : <Button type={'primary'} icon={<AppstoreAddOutlined/>}
+                              onClick={() => {
+                                  setCurrentRental({});
+                                  setRentalModalVisible(true);
+                              }
+                              }/>}
+            </div>}
             size='large'
             pagination={{
                 onChange: page => {
@@ -42,42 +89,7 @@ export function RentalsListComponent({ googleMapInstances }) {
             renderItem={rental => (
                 <List.Item
                     key={rental._id}
-                    actions={[
-                        <IconText title={'Date Added'} icon={CalendarOutlined}
-                                  text={new Date(rental.dateAdded).toDateString()}
-                                  key='size'/>,
-                        <IconText title={'Floor Area Size'} icon={ExpandAltOutlined} text={rental.floorAreaSize}
-                                  key='size'/>,
-                        <IconText title={'Price per month'} icon={DollarOutlined} text={rental.pricePerMonth}
-                                  key='price-per-month'/>,
-                        <IconText title={'Number of rooms'} icon={AppstoreOutlined} text={rental.roomsCount}
-                                  key='rooms'/>,
-                        <Button shape={'round'} type={'primary'} ghost icon={<EditOutlined/>}
-                                onClick={() => {
-                                    setCurrentRental(rental);
-                                    setRentalModalVisible(true);
-                                }}/>,
-                        <Popconfirm
-                            title='Sure to delete this rental?'
-                            onConfirm={() => {
-                                ajax.delete({ url: `rentals/${rental._id}` }).then(responseJson => {
-                                    setRentals(responseJson);
-                                });
-                            }}
-                            okText='Yes'
-                            cancelText='No'
-                        >
-                            <Button shape={'round'} type={'danger'} ghost icon={<DeleteOutlined/>}/>
-                        </Popconfirm>
-                    ]}
-                    extra={
-                        <img
-                            width={272}
-                            alt='logo'
-                            src='https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png'
-                        />
-                    }
-                >
+                    actions={getRentalActionSection(rental)}>
                     <List.Item.Meta
                         avatar={<Avatar>{rental.name[0].toUpperCase()}</Avatar>}
                         title={rental.name}
